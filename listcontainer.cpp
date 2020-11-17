17e6da9b08209c39e34e78f5e76739a76f70cc28
michaelvong <mvong006@ucr.edu>
2020-11-17 02:35:34 -0800

    Rearranged the file so that class is gone, implemented sort functions for both sorts, added set sort func, added header guards, added at func

--- listcontainer.cpp
+++ listcontainer.cpp
@@ -1,60 +1,73 @@
+#ifndef __LISTCONTAINER_CPP__
+#define __LISTCONTAINER_CPP__
+
 #include <list>
 #include <iterator>
 #include "base.hpp"
 #include "container.hpp"
 #include <iostream>
+#include "listcontainer.hpp"
 
 using namespace std; 
 
-class ListContainer : public Container {
-	protected:
-		list <Base*> mylist;
 
-	public:
-		ListContainer() : Container() {}
-		ListContainer(Sort* sort) : Container(sort) {}
+ListContainer::ListContainer(){}
+ListContainer::ListContainer(Sort* sort) : Container(sort) {}
 		
-		void add_element(Base* element){
-			mylist.push_back(element);
-		}
+void ListContainer::add_element(Base* element){
+	mylist.push_back(element);
+}
 		
-		void print(){
-			list<Base*> printList = mylist;
-			for (int i = 0; i < printList.size(); ++i){
-				cout << printList.front()->evaluate() << endl;
-				printList.pop_front();
-			}
-		}
+void ListContainer::print(){
+	list<Base*> printList = mylist;
+	for (int i = 0; i < printList.size(); ++i){
+		cout << printList.front()->evaluate() << endl;
+		printList.pop_front();
+	}
+}
 		
-		Base* at(int i){
-			if (mylist.size() > i){
-				list<Base*>::iterator it = mylist.begin();
-				std::advance(it, i);
-				return *it;
-			}
-		}
+Base* ListContainer::at(int i){
+	if (mylist.size() > i){
+		list<Base*>::iterator it = mylist.begin();
+		std::advance(it, i);
+		return *it;
+	}
+}
 		
-		int size(){
+		int ListContainer::size(){
 			return mylist.size();
 		}
 		
-		void swap(int i, int j){
-			if (mylist.size() > i && mylist.size() > j){
-				list<Base*>::iterator it = mylist.begin();
-				list<Base*>::iterator it1 = mylist.begin();
-				std::advance(it, i);
-				std::advance(it1, j);
+void ListContainer::swap(int i, int j){
+	if (mylist.size() > i && mylist.size() > j){
+		list<Base*>::iterator it = mylist.begin();
+		list<Base*>::iterator it1 = mylist.begin();
+		std::advance(it, i);
+		std::advance(it1, j);
 			
-				Base* temp3 = *it;
-				*it = *it1;
-				*it1 = temp3;
-			}
-		}
+		Base* temp3 = *it;
+		*it = *it1;
+		*it1 = temp3;
+	}
+}
 		
-		void sort(){
-			if (sort_function == NULL){
-				throw "No sort specified\n";
-				return;
-			}
-		}
-};
+void ListContainer::sort(){
+	if (thisSortFunction == NULL){
+		throw "No sort specified\n";
+		return;
+	}
+			
+	else if (thisSortFunction = new SelectionSort()){
+		thisSortFunction->sort(this);
+	}
+	else if (thisSortFunction = new BubbleSort()){
+		thisSortFunction->sort(this);
+	}
+}
+
+void ListContainer::set_sort_function(Sort* sort_function){
+	thisSortFunction = sort_function;
+}
+
+
+#endif //__LISTCONTAINER_CPP__
