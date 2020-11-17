a4022cb7404b3317f18b8dfa23f6734a1151ba39
isant028 <isabellasantiago2469@gmail.com>
2020-11-17 11:25:17 -0800

    added new test

---
+++ sortTest_test.hpp
@@ -0,0 +1,47 @@
+#ifndef __SORTSET_TEST_HPP__ 
+#define  __SORTSET_TEST_HPP__
+#include "gtest/gtest.h"
+
+#include "listcontainer.cpp"
+#include "op.hpp"
+#include "Mult.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "SelectionSort.cpp"
+#include "sort.hpp"
+#include "VectorContainer.hpp"
+
+TEST(SortASet, SelectionSortTest) {
+    Op* seven = new Op(7);
+    Op* four = new Op(4);
+    Mult* TreeA = new Mult(seven, four);
+
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Add* TreeB = new Add(three, two);
+
+    Op* ten = new Op(10);
+    Op* six = new Op(6);
+    Sub* TreeC = new Sub(ten, six);
+
+    VectorContainer* container = new VectorContainer();
+    container->add_element(TreeA);
+    container->add_element(TreeB);
+    container->add_element(TreeC);
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 28);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+    container->set_sort_function(new SelectionSort());
+    container->sort();
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 4);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 28);
+}
+
+#endif 
+        
