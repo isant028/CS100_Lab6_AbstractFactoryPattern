9e8c03ac72d70bcc448d3ead379d90caaa447c2f
michaelvong <mvong006@ucr.edu>
2020-11-17 02:36:12 -0800

    Added a unit test for selection sort and 3 unit tests for bubble sort, all successful

--- listcontainer_test.hpp
+++ listcontainer_test.hpp
@@ -5,6 +5,11 @@
 
 #include "listcontainer.cpp"
 #include "op.hpp"
+#include "Mult.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "SelectionSort.cpp"
+#include "sort.hpp"
 TEST(ListContainerTestSet, SwapTest){
 	Op* seven = new Op(7);
 	Op* nine = new Op(9);
@@ -18,7 +23,105 @@ TEST(ListContainerTestSet, SwapTest){
 	test_container->swap(0,1);
 	EXPECT_EQ(test_container->at(0)-> evaluate(), 9);
 	EXPECT_EQ(test_container->at(1)-> evaluate(), 7);
+}
+
+TEST(SortTestSet, SelectionSortTest){
+	Op* seven = new Op(7);
+	Op* four = new Op(4);
+	Mult* TreeA = new Mult(seven, four);
+
+	Op* three = new Op(3);
+	Op* two = new Op(2);
+	Add* TreeB = new Add(three, two);
+
+	Op* ten = new Op(10);
+	Op* six = new Op(6);
+	Sub* TreeC = new Sub(ten, six);
+
+	ListContainer* test_container = new ListContainer();
+	test_container->add_element(TreeA);
+	test_container->add_element(TreeB);
+	test_container->add_element(TreeC);
+
+	ASSERT_EQ(test_container->size(), 3);
+	EXPECT_EQ(test_container->at(0)->evaluate(), 28);
+	EXPECT_EQ(test_container->at(1)->evaluate(), 5);
+	EXPECT_EQ(test_container->at(2)->evaluate(), 4);
+
+
+	test_container->set_sort_function(new SelectionSort());
+	test_container->sort();
+	ASSERT_EQ(test_container->size(), 3);
+	EXPECT_EQ(test_container->at(0)->evaluate(), 4);
+	EXPECT_EQ(test_container->at(1)->evaluate(), 5);
+	EXPECT_EQ(test_container->at(2)->evaluate(), 28);	
 
 }
 
+
+TEST(SortTestSet, BubbleSortTest){
+	Op* seven = new Op(9);
+	Op* four = new Op(4);
+	Mult* TreeA = new Mult(seven, four);
+
+	Op* three = new Op(36);
+	Op* two = new Op(2);
+	Add* TreeB = new Add(three, two);
+
+	Op* ten = new Op(10);
+	Op* six = new Op(4);
+	Sub* TreeC = new Sub(ten, six);
+
+	ListContainer* test_container1 = new ListContainer();
+	test_container1->add_element(TreeA);
+	test_container1->add_element(TreeB);
+	test_container1->add_element(TreeC);
+
+	ASSERT_EQ(test_container1->size(), 3);
+	EXPECT_EQ(test_container1->at(0)->evaluate(), 36);
+	EXPECT_EQ(test_container1->at(1)->evaluate(), 38);
+	EXPECT_EQ(test_container1->at(2)->evaluate(), 6);
+
+
+	test_container1->set_sort_function(new BubbleSort());
+	test_container1->sort();
+	ASSERT_EQ(test_container1->size(), 3);
+	EXPECT_EQ(test_container1->at(0)->evaluate(), 6);
+	EXPECT_EQ(test_container1->at(1)->evaluate(), 36);
+	EXPECT_EQ(test_container1->at(2)->evaluate(), 38);	
+
+}
+
+TEST(SortTestSet, BubbleSortTestNegative){
+	Op* NegNine = new Op(-9);
+	Op* one = new Op(1);
+	Mult* TreeA = new Mult(NegNine, one);
+
+	Op* five = new Op(5);
+	Op* Negtwo = new Op(-2);
+	Add* TreeB = new Add(five, Negtwo);
+
+	Op* six = new Op(6);
+	Op* NegFour = new Op(-4);
+	Sub* TreeC = new Sub(six, NegFour);
+
+	ListContainer* test_container1 = new ListContainer();
+	test_container1->add_element(TreeA);
+	test_container1->add_element(TreeB);
+	test_container1->add_element(TreeC);
+
+	ASSERT_EQ(test_container1->size(), 3);
+	EXPECT_EQ(test_container1->at(0)->evaluate(), -9);
+	EXPECT_EQ(test_container1->at(1)->evaluate(), 3);
+	EXPECT_EQ(test_container1->at(2)->evaluate(), 10);
+
+
+	test_container1->set_sort_function(new BubbleSort());
+	test_container1->sort();
+	ASSERT_EQ(test_container1->size(), 3);
+	EXPECT_EQ(test_container1->at(0)->evaluate(), -9);
+	EXPECT_EQ(test_container1->at(1)->evaluate(), 3);
+	EXPECT_EQ(test_container1->at(2)->evaluate(), 10);	
+
+}
 #endif //__LISTCONTAINER_TEST_HPP__
